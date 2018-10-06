02e671147936e06ccd1d65bd920de54742fe2db4
rvanhorn <41927843+rvanhorn@users.noreply.github.com>
2018-10-06 16:47:54 -0700

    Update API calls to work with new layout.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,46 +1,46 @@
-$(document).ready(function() {
-  let videos = [
-    "antman.mp4",
-    "Deadpool.mp4",
-    "hulk.mp4",
-    "panther.mp4",
-    "strange.mp4",
-    "scarlett.mp4",
-    "spiderman.mp4",
-    "thanos.mp4"
-  ];
+$(document).ready(function () {
+	let videos = [
+		"antman.mp4",
+		"Deadpool.mp4",
+		"hulk.mp4",
+		"panther.mp4",
+		"strange.mp4",
+		"scarlett.mp4",
+		"spiderman.mp4",
+		"thanos.mp4"
+	];
 
-  let displayArray = shuffle(videos);
-  let i = 0;
+	let displayArray = shuffle(videos);
+	let i = 0;
 
-  $("#myVideo").attr("src", "./video/" + displayArray[i]);
+	$("#myVideo").attr("src", "./video/" + displayArray[i]);
 
-  //finsihed
-  document.getElementById("myVideo").addEventListener(
-    "ended",
-    function() {
-      if (i >= displayArray.length) {
-        console.log("reset");
+	//finsihed
+	document.getElementById("myVideo").addEventListener(
+		"ended",
+		function () {
+			if (i >= displayArray.length) {
+				console.log("reset");
 
-        i = 0;
-        displayArray = shuffle(videos);
-        $("#myVideo").attr("src", "./video/" + displayArray[i]);
-      } else {
-        i++;
-        $("#myVideo").attr("src", "./video/" + displayArray[i]);
-      }
-    },
-    false
-  );
+				i = 0;
+				displayArray = shuffle(videos);
+				$("#myVideo").attr("src", "./video/" + displayArray[i]);
+			} else {
+				i++;
+				$("#myVideo").attr("src", "./video/" + displayArray[i]);
+			}
+		},
+		false
+	);
 });
 
 //shuffle array
 function shuffle(a) {
-  for (let i = a.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [a[i], a[j]] = [a[j], a[i]];
-  }
-  return a;
+	for (let i = a.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[a[i], a[j]] = [a[j], a[i]];
+	}
+	return a;
 }
 
 // Global Variables
@@ -50,203 +50,234 @@ let marvelURL = "https://gateway.marvel.com:443/v1/public/characters";
 let searchLimit = 50;
 let extension;
 
+let searchInput;
 let characterID;
-let apiURL;
-let urlType;
 
-let comicsURL =
-  marvelURL + characterID + "/comics?formatType=comic&noVariants=true&hasDigitalIssue=true&apikey=" + apiKey;
+let characterFirstAppreance;
+let firstIssue;
+let characterCreators = [];
 
 // API Calls
 
-$("#search-btn").on("click", function(event) {
-  event.preventDefault();
-  let searchInput = $("#search-box").val();
-  console.log($("#search-btn").val());
+$("#search-btn").on("click", function (event) {
 
-  //move dom elements on click
-  $(".search-box-div").css({
-    position: "absolute",
-    width: "25%",
-    top: "15px",
-    right: "0px"
-  });
+	event.preventDefault();
+	searchInput = $("#search-box").val();
+	getCharacterData();
 
-  apiURL =
-    marvelURL + "?ts=1&nameStartsWith=" + searchInput + "&limit=" + searchLimit + "&apikey=" + apiKey + "&hash=" + hash;
-  urlType = "Character";
-  $("#character-images").empty();
-  getAPIData();
+	//move dom elements on click
+	$(".search-box-div").css({
+		position: "absolute",
+		width: "25%",
+		top: "15px",
+		right: "0px"
+
+	});
 });
 
-$(document).on("click", ".image-container", function() {
-  characterID = $(this).attr("character-id");
-  getEventData();
+
+$("#search-btn-nav").on("click", function (event) {
+
+	event.preventDefault();
+	let searchInput = $("#search-box-nav").val();
+
+	//move dom elements on click
+	$(".search-box-div").css({
+		position: "absolute",
+		width: "25%",
+		top: "15px",
+		right: "0px"
+	});
 });
-
-function getEventData() {
-  apiURL = marvelURL + "/" + characterID + "/events?ts=1&limit=" + searchLimit + "&apikey=" + apiKey + "&hash=" + hash;
-  urlType = "Event";
-  $("#event-images").empty();
-  getAPIData();
-}
-
-function getAPIData() {
-  console.log(apiURL);
-  console.log(urlType);
-
-  $.ajax({
-    async: true,
-    cache: true,
-    url: apiURL,
-    method: "GET"
-  }).then(function(results) {
-    console.log(results);
-    $("#results-row").empty();
-    for (let i = 0; i < results.data.results.length; i++) {
-      let typeWrapper = $("<div>");
-      let typeName = $("<p>");
-      let typeImage = $("<img>");
-
-      extension = "." + results.data.results[i].thumbnail.extension;
-
-      typeImage.attr("class", "image-container");
-      typeImage.attr("src", results.data.results[i].thumbnail.path + extension);
-
-      if (urlType === "Character") {
-        typeName.attr("class", "character-name");
-        typeName.text(results.data.results[i].name);
-        typeImage.attr("character-id", results.data.results[i].id);
-        typeWrapper.attr("class", "col-xl-3");
-        typeWrapper.append(typeName, typeImage);
-        $("#results-row").append(typeWrapper);
-      }
-
-      if (urlType === "Comic") {
-        typeName.attr("class", "comic-name");
-        typeName.text(results.data.results[i].title);
-        typeImage.attr("comic-id", results.data.results[i].id);
-        typeWrapper.append(typeName, typeImage);
-        $("#comic-images").append(typeWrapper);
-      }
-
-      if (urlType === "Event") {
-        typeName.attr("class", "event-name");
-        typeName.text(results.data.results[i].title);
-        typeImage.attr("event-id", results.data.results[i].id);
-        typeWrapper.append(typeName, typeImage);
-        $("#event-images").append(typeWrapper);
-      }
-    }
-  });
-}
-
-$("#search-btn-nav").on("click", function(event) {
-  event.preventDefault();
-  let searchInput = $("#search-box-nav").val();
-  console.log($("#search-btn").val());
-
-  //move dom elements on click
-  $(".search-box-div").css({
-    position: "absolute",
-    width: "25%",
-    top: "15px",
-    right: "0px"
-  });
-
-  apiURL =
-    marvelURL + "?ts=1&nameStartsWith=" + searchInput + "&limit=" + searchLimit + "&apikey=" + apiKey + "&hash=" + hash;
-  urlType = "Character";
-  $("#character-images").empty();
-  getAPIData();
-});
-
-$(document).on("click", ".image-container", function() {
-  characterID = $(this).attr("character-id");
-  getEventData();
-});
-
-function getEventData() {
-  apiURL = marvelURL + "/" + characterID + "/events?ts=1&limit=" + searchLimit + "&apikey=" + apiKey + "&hash=" + hash;
-  urlType = "Event";
-  $("#event-images").empty();
-  getAPIData();
-}
-
-function getAPIData() {
-  console.log(apiURL);
-  console.log(urlType);
-
-  $.ajax({
-    async: true,
-    cache: true,
-    url: apiURL,
-    method: "GET"
-  }).then(function(results) {
-    console.log(results);
-    $("#results-row").empty();
-    for (let i = 0; i < results.data.results.length; i++) {
-      let typeWrapper = $("<div>");
-      let typeName = $("<p>");
-      let typeImage = $("<img>");
-
-      extension = "." + results.data.results[i].thumbnail.extension;
-
-      typeImage.attr("class", "image-container");
-      typeImage.attr("src", results.data.results[i].thumbnail.path + extension);
-
-      if (urlType === "Character") {
-        typeName.attr("class", "character-name");
-        typeName.text(results.data.results[i].name);
-        typeImage.attr("character-id", results.data.results[i].id);
-        typeWrapper.attr("class", "col-xl-3");
-        typeWrapper.append(typeName, typeImage);
-        $("#results-row").append(typeWrapper);
-      }
-
-      if (urlType === "Comic") {
-        typeName.attr("class", "comic-name");
-        typeName.text(results.data.results[i].title);
-        typeImage.attr("comic-id", results.data.results[i].id);
-        typeWrapper.append(typeName, typeImage);
-        $("#comic-images").append(typeWrapper);
-      }
-
-      if (urlType === "Event") {
-        typeName.attr("class", "event-name");
-        typeName.text(results.data.results[i].title);
-        typeImage.attr("event-id", results.data.results[i].id);
-        typeWrapper.append(typeName, typeImage);
-        $("#event-images").append(typeWrapper);
-      }
-    }
-  });
-}
 
 // Loading Animations
 
-$(document).ajaxStart(function() {
-  $("#loading-wrapper").css({
-    opacity: "1",
-    left: "0"
-  });
+$(document).ajaxStart(function () {
+	$("#loading-wrapper").css({
+		opacity: "1",
+		left: "0"
+	});
 });
 
-$(document).ajaxComplete(function() {
-  setTimeout(function() {
-    $("#loading-wrapper").css({
-      opacity: "0",
-      left: "-100%"
-    });
+$(document).ajaxComplete(function () {
+	setTimeout(function () {
+		$("#loading-wrapper").css({
+			opacity: "0",
+			left: "-100%"
+		});
 
-    $("#homepage-div").css({
-      display: "none"
-    });
-    $("#results-div").css({
-      display: "block"
-    });
-  }, 3000);
-  $("#search-content-div").css({
-    opacity: "1"
-  });
+		$("#homepage-div").css({
+			display: "none"
+		});
+		$("#results-div").css({
+			display: "block"
+		});
+	}, 3000);
+	$("#search-content-div").css({
+		opacity: "1"
+	});
+
 });
+
+
+//--------------------------------
+// Marvel API Calls
+//--------------------------------
+
+$(document).on("click", ".image-container", function () {
+
+	characterID = $(this).attr("character-id");
+	getComicData();
+	getEventData();
+	getCharacterDetailData();
+
+});
+
+function getCharacterData() {
+
+	$("#character-images").empty();
+	$("#comic-images").empty();
+	$("#event-images").empty();
+
+	$.ajax({
+
+		cache: true,
+		url: marvelURL + "?ts=1&nameStartsWith=" + searchInput + "&limit=" + searchLimit + "&apikey=" + apiKey + "&hash=" + hash,
+		method: "GET"
+
+	}).then(function (character) {
+
+		console.log(character);
+
+		for (let i = 0; i < character.data.results.length; i++) {
+
+			let typeWrapper = $("<div>");
+			let typeName = $("<p>");
+			let typeImage = $("<img>");
+
+			extension = "." + character.data.results[i].thumbnail.extension;
+
+			typeImage.attr("class", "image-container");
+			typeImage.attr("src", character.data.results[i].thumbnail.path + extension);
+
+			typeName.attr("class", "character-name");
+			typeName.text(character.data.results[i].name);
+			typeImage.attr("character-id", character.data.results[i].id);
+			typeWrapper.append(typeName, typeImage);
+			$("#results-row").append(typeWrapper);
+
+		}
+	})
+}
+
+function getComicData() {
+
+	$.ajax({
+
+		cache: true,
+		url: marvelURL + "/" + characterID + "/comics?orderBy=onsaleDate&formatType=comic&noVariants=false&hasDigitalIssue=true&limit=" + searchLimit + "&apikey=" + apiKey + "&hash=" + hash,
+		method: "GET"
+
+	}).then(function (comic) {
+
+		console.log("Comic");
+		console.log(comic);
+
+		for (let i = 0; i < comic.data.results.length; i++) {
+
+			let typeWrapper = $("<div>");
+			let typeName = $("<p>");
+			let typeImage = $("<img>");
+
+			extension = "." + comic.data.results[i].thumbnail.extension;
+
+			typeImage.attr("class", "image-container");
+			typeImage.attr("src", comic.data.results[i].thumbnail.path + extension);
+
+			typeName.attr("class", "comic-name");
+			typeName.text(comic.data.results[i].title);
+			typeImage.attr("comic-id", comic.data.results[i].id);
+			typeWrapper.append(typeName, typeImage);
+			$("#comic-images").append(typeWrapper);
+
+		}
+	})
+}
+
+function getEventData() {
+
+	$.ajax({
+
+		cache: true,
+		url: marvelURL + "/" + characterID + "/events?ts=1&limit=" + searchLimit + "&apikey=" + apiKey + "&hash=" + hash,
+		method: "GET"
+
+	}).then(function (event) {
+
+		console.log(event);
+
+		for (let i = 0; i < event.data.results.length; i++) {
+
+			let typeWrapper = $("<div>");
+			let typeName = $("<p>");
+			let typeImage = $("<img>");
+
+			extension = "." + event.data.results[i].thumbnail.extension;
+
+			typeImage.attr("class", "image-container");
+			typeImage.attr("src", event.data.results[i].thumbnail.path + extension);
+
+			typeName.attr("class", "event-name");
+			typeName.text(event.data.results[i].title);
+			typeImage.attr("event-id", event.data.results[i].id);
+			typeWrapper.append(typeName, typeImage);
+			$("#event-images").append(typeWrapper);
+
+		}
+	})
+}
+
+function getCharacterDetailData() {
+
+	$.ajax({
+		cache: true,
+		url: marvelURL + "/" + characterID + "/comics?ts=1&orderBy=onsaleDate&limit=" + searchLimit + "&apikey=" + apiKey + "&hash=" + hash,
+		method: "GET"
+
+	}).then(function (characterDetail) {
+
+		console.log(characterDetail);
+
+		for (let i = 0; i < characterDetail.data.results.length; i++) {
+
+			if ((characterDetail.data.results[i].dates[0].date).indexOf("-0001")) {
+
+				if (characterFirstAppreance === undefined) {
+
+					characterFirstAppreance = characterDetail.data.results[i].dates[0].date;
+					characterFirstAppreance = characterFirstAppreance.substring(0, 4);
+					console.log(characterFirstAppreance);
+
+					if (firstIssue === undefined) {
+
+						firstIssue = i;
+
+					}
+
+				}
+
+				for (let n = 0; n < characterDetail.data.results[firstIssue].creators.items.length; n++) {
+
+					if (!characterCreators.includes(characterDetail.data.results[firstIssue].creators.items[n].name)) {
+
+						characterCreators.push(characterDetail.data.results[firstIssue].creators.items[n].name);
+						console.log(characterCreators);
+
+					}
+
+				}
+			}
+		}
+
+	})
+}
